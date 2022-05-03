8e1e53d1b718427102cbed39443e61d2039d0f2c
demKyr <64605968+demKyr@users.noreply.github.com>
2022-05-03 11:23:50 +0300

    Redirect to login page if not connected to Metamask

--- client/components/layout/layout.js
+++ client/components/layout/layout.js
@@ -1,13 +1,21 @@
 import { Fragment } from "react";
+import { useWeb3React } from "@web3-react/core";
+
 import MainHeader from "./main-header";
+import LoginPage from "../../pages/login";
 
 function Layout(props) {
-  return (
-    <Fragment>
-      <MainHeader />
-      <main>{props.children}</main>
-    </Fragment>
-  );
+  const { activate, active } = useWeb3React();
+  if (active) {
+    return (
+      <Fragment>
+        <MainHeader />
+        <main>{props.children}</main>
+      </Fragment>
+    );
+  } else {
+    return <LoginPage />;
+  }
 }
 
 export default Layout;
