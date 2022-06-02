77b501a54a54afd5d4444f53721d7641cade5b2f
demKyr <64605968+demKyr@users.noreply.github.com>
2022-06-02 17:06:42 +0300

    Parameterize API path

--- client/constants/parameters.js
+++ client/constants/parameters.js
@@ -1,5 +1,7 @@
 module.exports = {
-    ModelCost : "1",
-    CaptionCost : "0.001",
-    ReviewCost : "0.001",
-}
+  ModelCost: "1",
+  CaptionCost: "0.001",
+  ReviewCost: "0.001",
+  // APIpath: "http://9aa7-34-74-37-114.ngrok.io",
+  APIpath: "http://127.0.0.1:5000",
+};
