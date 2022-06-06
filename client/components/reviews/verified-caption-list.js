f41212ee93728dfe2722c5790f78a3d5af29cb22
demKyr <64605968+demKyr@users.noreply.github.com>
2022-06-06 19:57:34 +0300

    Connect verified-caption-list to /train & /evaluate API routes and updateCaptionTrainStatus & addAccuracy

--- client/components/reviews/verified-caption-list.js
+++ client/components/reviews/verified-caption-list.js
@@ -1,27 +1,119 @@
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import { Fragment, useContext } from "react";
+
+import ContractsContext from "../../store/contract-context";
+import { APIpath } from "../../constants/parameters";
+import Button from "../ui/button";
 import VerifiedCaption from "./verified-caption";
 import ModalComponent from "./subcomponents/modal-component";
-
+import classes from "./verified-caption-list.module.css";
 
 function VerifiedCaptionList(props) {
+  const { activate, active, library: provider } = useWeb3React();
+  const contractsCtx = useContext(ContractsContext);
+
+  async function saveAccuracy(modelId, acc) {
+    if (active) {
+      try {
+        let intAcc = parseInt(acc * 100000);
+        await contractsCtx.contracts["submitReview"].addAccuracy(
+          modelId,
+          intAcc
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      document.getElementById("executeButton").innerHTML =
+        "Please install Metamask";
+    }
+  }
+
+  async function hideUsedCaptions(usedCaptions) {
+    if (active) {
+      try {
+        let usedCaptionsIds = [];
+        for (const key in usedCaptions) {
+          usedCaptionsIds.push(parseInt(usedCaptions[key]["id"]));
+        }
+        await contractsCtx.contracts["submitReview"].updateCaptionTrainStatus(
+          usedCaptionsIds
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      document.getElementById("executeButton").innerHTML =
+        "Please install Metamask";
+    }
+  }
+
+  function trainHandler(event) {
+    event.preventDefault();
+
+    if (active) {
+      try {
+        let trainData = props.myCaptions;
+        // Create request for Training
+        const reqOptions = {
+          method: "POST",
+          body: JSON.stringify(trainData),
+        };
+        // Make API call for training
+        fetch(APIpath + "/train", reqOptions)
+          .then((res) => res.json())
+          .then((contents) => {
+            if (contents["status"] == "ok") {
+              //Make API call for evaluation to receive loss and accuracy
+              try {
+                fetch(APIpath + "/evaluate")
+                  .then((res) => res.json())
+                  .then((contents) => {
+                    let acc = contents["acc"];
+                    let loss = contents["loss"];
+                    saveAccuracy(props.myModelId, acc);
+                    hideUsedCaptions(trainData);
+                  });
+              } catch (error) {
+                console.log(error);
+              }
+            }
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      document.getElementById("executeButton").innerHTML =
+        "Please install Metamask";
+    }
+  }
+
   return (
     <ul>
       <ModalComponent
         myName={props.myName}
         myDescription={props.myDescription}
       />
-      {!props.myCaptions.length && (
+      {!props.myCaptions.length ? (
         <p className="noOutputMsg">No captions available for this model</p>
+      ) : (
+        <Fragment>
+          {props.myCaptions.map((caption) => (
+            <VerifiedCaption
+              key={caption.id}
+              myCaption={caption.content}
+              myProposedLbl={caption.proposedLbl}
+              myVerifiedLbl={caption.verifiedLbl}
+              myGoodData={caption.goodData}
+              myLabels={props.myLabels}
+            />
+          ))}
+          <div className={classes.trainButton}>
+            <Button onClick={trainHandler}>Train Model</Button>
+          </div>
+        </Fragment>
       )}
-      {props.myCaptions.map((caption) => (
-        <VerifiedCaption
-          key={caption.id}
-          myCaption={caption.content}
-          myProposedLbl={caption.proposedLbl}
-          myVerifiedLbl={caption.verifiedLbl}
-          myGoodData={caption.goodData}
-          myLabels={props.myLabels}
-        />
-      ))}
     </ul>
   );
 }
--- client/constants/parameters.js
+++ client/constants/parameters.js
@@ -2,6 +2,6 @@ module.exports = {
   ModelCost: "1",
   CaptionCost: "0.001",
   ReviewCost: "0.001",
-  APIpath: "http://3cce-35-247-82-164.ngrok.io",
+  APIpath: "http://5256-104-198-180-226.ngrok.io",
   // APIpath: "http://127.0.0.1:5000",
 };
