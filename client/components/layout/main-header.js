d4de322f141bbefa45c73a5db38a61d5b9a04bb4
demKyr <64605968+demKyr@users.noreply.github.com>
2022-06-08 13:37:12 +0300

    Add logo to nav bar

--- client/components/layout/main-header.js
+++ client/components/layout/main-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import classes from "./main-header.module.css";
 
@@ -7,26 +8,72 @@ function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">Home page</Link>
+        <Link href="/">
+          <a>
+            <Image
+              src="/Thesis_Logo_2.png"
+              alt="home"
+              width="100"
+              height="100"
+            />
+          </a>
+        </Link>
       </div>
+
       <nav>
         <ul>
-          <li className={(localPathname == "/request") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/request"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/request">Request</Link>
           </li>
-          <li className={(localPathname == "/submit") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/submit"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/submit">Submit</Link>
           </li>
-          <li className={(localPathname == "/review") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/review"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/review">Review</Link>
           </li>
-          <li className={(localPathname == "/predict") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/predict"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/predict">Predict</Link>
           </li>
-          <li className={(localPathname == "/verified") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/verified"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/verified">Train</Link>
           </li>
-          <li className={(localPathname == "/evaluation") ? classes.selectedTab : classes.nonSelectedTab}>
+          <li
+            className={
+              localPathname == "/evaluation"
+                ? classes.selectedTab
+                : classes.nonSelectedTab
+            }
+          >
             <Link href="/evaluation">Evaluation</Link>
           </li>
         </ul>
