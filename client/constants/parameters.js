b7b7b324b7427aecc98443db2286e49248b7681e
demKyr <demetris_k@hotmail.com>
2022-06-23 12:01:51 +0300

    Clean FlaskServerForBert.ipynb

--- client/constants/parameters.js
+++ client/constants/parameters.js
@@ -2,6 +2,6 @@ module.exports = {
   ModelCost: "1",
   CaptionCost: "0.001",
   ReviewCost: "0.001",
-  APIpath: "http://fa95-35-247-120-69.ngrok.io",
+  APIpath: "http://59fb-34-75-117-178.ngrok.io",
   // APIpath: "http://127.0.0.1:5000",
 };
