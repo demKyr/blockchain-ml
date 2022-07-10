766ae04ff88420f4114ec3787972278f6fb7a1b6
demKyr <demetris_k@hotmail.com>
2022-07-10 22:09:18 +0300

    Update Documentation

--- client/constants/parameters.js
+++ client/constants/parameters.js
@@ -2,6 +2,6 @@ module.exports = {
   ModelCost: "1",
   CaptionCost: "0.001",
   ReviewCost: "0.001",
-  APIpath: "http://c773-35-230-113-195.ngrok.io",
+  APIpath: "http://b502-34-142-221-65.ngrok.io",
   // APIpath: "http://127.0.0.1:5000",
 };
